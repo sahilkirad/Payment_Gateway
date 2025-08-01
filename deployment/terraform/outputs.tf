d6ff91652983e72f3adcd855797f580afd479fb9
Arumulla Yaswanth Reddy <yaswanth.arumulla@gmail.com>
2025-08-01 15:33:49 +0530

    terraform changes-1

--- deployment/terraform/outputs.tf
+++ deployment/terraform/outputs.tf
@@ -1,16 +1,8 @@
-output "eks_cluster_name" {
-  value = module.eks.cluster_name
+output "region" {
+  description = "Jumphost Server region"
+  value       = var.region
 }
-
-output "ray_head_node_group" {
-  value = module.eks.eks_managed_node_groups["ray_head"].node_group_name
-}
-
-output "ray_worker_node_group" {
-  value = module.eks.eks_managed_node_groups["ray_worker"].node_group_name
-}
-
-output "kubeconfig" {
-  value       = module.eks.kubeconfig_filename
-  description = "Path to generated kubeconfig file"
+output "jumphost_public_ip" {
+  description = "Public IP address of the EC2 jumphost"
+  value       = aws_instance.ec2.public_ip
 }
